637368087af7203d4e47254a18c7383415276bd2
jaycee808 <jamie.clark@live.co.uk>
2024-05-17 16:18:22 +0100

    updated course unit json file and displayed unit details on course page

--- app/courseUnits/page.jsx
+++ app/courseUnits/page.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import units from '../units.json'
 
 const CourseUnitsPage = () => {
     return (
         <div>
-            <h2>This is the Course Units Page</h2>
-            <Link href="/">Go Home</Link>
+        <h2>This is the Course Units Page</h2>
+        <Link href="/">Go Home</Link>
+        
+        {units.map(unit => (
+            <div key={unit._id}>
+            <h2>{unit.title}</h2>
+            <p>{unit.teacher}</p>
+            <p>{unit.overview}</p>
+            <ul>
+                {unit.schedule.map((scheduleItem, index) => (
+                <li key={index}>
+                    {scheduleItem.classType}
+                    {scheduleItem.dayOfWeek}  
+                    {scheduleItem.classStart}
+                    {scheduleItem.classEnd}
+                </li>
+                ))}
+            </ul>
+            </div>
+        ))}
         </div>
-    )
-}
+    );
+};
 
 export default CourseUnitsPage;

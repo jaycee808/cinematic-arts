a43c4469c443cdefe05d19423ae35bde4551bb00
jaycee808 <jamie.clark@live.co.uk>
2024-05-20 16:46:05 +0100

    added select unit function

--- app/courseUnits/page.jsx
+++ app/courseUnits/page.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
-import units from '../units.json'
+import units from '../units.json';
 
 const CourseUnitsPage = () => {
+    const [selectedUnit, setSelectedUnit] = useState(null);
+
+    const handleSelectUnit = (unitId, unitTitle) => {
+        setSelectedUnit(unitId);
+        console.log(`Selected unit: ${unitTitle}`);
+    };
+
     return (
         <div>
-        <h2>This is the Course Units Page</h2>
-        <Link href="/">Go Home</Link>
-        
-        {units.map(unit => (
-            <div key={unit._id}>
-            <h2>{unit.title}</h2>
-            <p>{unit.teacher}</p>
-            <p>{unit.overview}</p>
-            <ul>
-                {unit.schedule.map((scheduleItem, index) => (
-                <li key={index}>
-                    {scheduleItem.classType}
-                    {scheduleItem.dayOfWeek}  
-                    {scheduleItem.classStart}
-                    {scheduleItem.classEnd}
-                </li>
-                ))}
-            </ul>
-            </div>
-        ))}
+            <h2>This is the Course Units Page</h2>
+            <Link href="/">Go Home</Link>
+
+            {units.map(unit => (
+                <div key={unit._id}>
+                    <h2>{unit.title}</h2>
+                    <p>{unit.teacher}</p>
+                    <p>{unit.overview}</p>
+                    <ul>
+                        {unit.schedule.map((scheduleItem, index) => (
+                            <li key={index}>
+                                {scheduleItem.classType}
+                                {scheduleItem.dayOfWeek}  
+                                {scheduleItem.classStart}
+                                {scheduleItem.classEnd}
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={() => handleSelectUnit(unit._id, unit.title)}>
+                        Select
+                    </button>
+                </div>
+            ))}
+            {selectedUnit && (
+                <div>
+                    <h3>You've selected unit: {selectedUnit}</h3>
+                    {}
+                </div>
+            )}
         </div>
     );
 };

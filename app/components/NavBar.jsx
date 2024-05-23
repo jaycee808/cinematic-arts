93a8e1df8b77658551192f161f5096fe932d0f9c
jaycee808 <jamie.clark@live.co.uk>
2024-05-23 11:08:12 +0100

    updated navbar styles and imported fonts

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -13,12 +13,12 @@ const NavBar = () => {
     return (
         <nav className="bg-slate-950 text-slate-100 p-4">
             <div className="flex items-center justify-between">
-                <div className="logo">
-                    <Link href="/">Cinematic Arts</Link>
+                <div className="logo text-6xl font-bold">
+                    <Link href="/">CINEMATIC ARTS.</Link>
                 </div> 
-                <div className={`nav-menu px-4 ${isMenuOpen ? 'block' : 'hidden'} md:flex items-center space-x-4`}>
-                    <Link href="/courseUnits">Course Units</Link>
-                    <Link href="/timetable">Timetable</Link>
+                <div className={`nav-menu text-2xl px-10 ${isMenuOpen ? 'block' : 'hidden'} md:flex items-center space-x-12`}>
+                    <Link href="/courseUnits">COURSE UNITS</Link>
+                    <Link href="/timetable">TIMETABLE</Link>
                 </div>
                 <button type="button" className="p-2 md:hidden" onClick={toggleMenu}>
                     <svg

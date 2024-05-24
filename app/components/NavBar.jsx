c1196e42b827e1c90c1621b93ccfecd3395c2582
jaycee808 <jamie.clark@live.co.uk>
2024-05-24 21:46:56 +0100

    imported fonts

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
     return (
         <nav className="relative p-4 bg-black shadow">
             <div className="flex items-center justify-between">
-                <div className="logo text-5xl lg:text-8xl text-white font-logo">
+                <div id="logo"className="logo text-5xl lg:text-6xl text-white font-bold">
                     <Link href="/">CINEMATIC ARTS<span className="text-red-900">.</span></Link>
                 </div> 
                 <div className="md:hidden">
@@ -37,7 +37,7 @@ const NavBar = () => {
             <div className={`fixed inset-0 bg-black bg-opacity-50 z-10 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={toggleMenu}></div>
             <div className={`fixed top-0 left-0 w-3/4 max-w-xs h-full bg-black z-20 p-4 transition-transform duration-300 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
                 <div className="flex flex-col space-y-4 text-white">
-                    <div className="logo text-5xl text-white font-logo" onClick={toggleMenu}>
+                    <div className="logo text-5xl text-white font-bold" onClick={toggleMenu}>
                         <Link href="/">CINEMATIC ARTS<span className="text-red-900">.</span></Link>
                     </div>
                     <Link href="/courseUnits" className="nav-link text-3xl font-bold" onClick={toggleMenu}>COURSE UNITS</Link>

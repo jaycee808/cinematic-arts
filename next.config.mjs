f00a3815b98afd6b9fc5469503248e89de51cd7c
jaycee808 <jamie.clark@live.co.uk>
2024-07-02 13:56:53 +0100

    updated next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    env: {
+        MONGODB_URI: process.env.MONGODB_URI,
+        NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+        NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+        NEXT_PUBLIC_PROD_DOMAIN: process.env.NEXT_PUBLIC_PROD_DOMAIN,
+        NEXT_PUBLIC_PROD_API: process.env.NEXT_PUBLIC_PROD_API,
+    },
+};
 
 export default nextConfig;

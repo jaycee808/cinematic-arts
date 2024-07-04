6322d0678c2ed82c984690034b51c29c80698399
jaycee808 <jamie.clark@live.co.uk>
2024-07-04 21:29:19 +0100

    updated nav bar

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,48 +1,18 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 const NavBar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
     return (
         <nav className="relative p-2 shadow bg-slate-50 text-black">
-            <div className="flex items-center justify-between">
-                <div id="logo" className="logo text-5xl lg:text-8xl font-bold">
+            <div className="flex flex-col items-center">
+                <div id="logo" className="logo text-5xl lg:text-8xl font-bold mb-4">
                     <Link href="/">CINEMATIC ARTS<span className="text-red-700">.</span></Link>
                 </div>
-                <div className="md:hidden">
-                    <button type="button" className="p-2" onClick={toggleMenu}>
-                        <svg
-                            viewBox="0 0 100 80"
-                            width="40"
-                            height="20"
-                            className="fill-current text-slate-950">
-                            <rect width="100" height="10"></rect>
-                            <rect y="30" width="75" height="10"></rect>
-                            <rect y="60" width="50" height="10"></rect>
-                        </svg>
-                    </button>
-                </div>
-                <div className="hidden md:flex items-center space-x-12">
-                    <Link href="/courseUnits" className="nav-link text-2xl font-bold">Course Units</Link>
-                    <Link href="/timetable" className="nav-link text-2xl font-bold">Timetable</Link>
-                </div>
-            </div>
-            <div className={`fixed inset-0 bg-slate-950 bg-opacity-50 z-10 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={toggleMenu}></div>
-            <div className={`fixed top-0 left-0 w-4/5 max-w-xs h-full bg-slate-950 text-slate-50 z-20 p-4 transition-transform duration-300 transform ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-                <div className="flex flex-col space-y-4">
-                    <div className="logo text-5xl font-bold" onClick={toggleMenu}>
-                        <Link href="/">CINEMATIC ARTS<span className="text-red-700">.</span></Link>
-                    </div>
-                    <hr></hr>
-                    <Link href="/courseUnits" className="nav-link text-2xl font-bold" onClick={toggleMenu}>Course Units</Link>
-                    <Link href="/timetable" className="nav-link text-2xl font-bold" onClick={toggleMenu}>Timetable</Link>
+                <div className="flex items-center space-x-12">
+                    <Link href="/courseUnits" className="nav-link text-2xl font-bold uppercase">Course Units</Link>
+                    <Link href="/timetable" className="nav-link text-2xl font-bold uppercase">Timetable</Link>
                 </div>
             </div>
         </nav>
--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,8 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import connectDatabase from '@/config/database'
 
-const HomePage = async () => {
-
-    await connectDatabase();
+const HomePage = () => {
 
     return (
         <div className="min-h-screen flex flex-col">
